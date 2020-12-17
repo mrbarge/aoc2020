07b02552eb552cbd3340361d17664aef0129e522
mrbarge <matt.bargenquast@gmail.com>
2020-12-17 21:47:07 +1000

    Day 17 added

--- day17/main.go
+++ day17/main.go
@@ -6,24 +6,125 @@ import (
 	"os"
 )
 
-func partOne(data []string) (int, error) {
+type Coord4D struct {
+	X int
+	Y int
+	Z int
+	W int
+	V int
+}
 
-	return 0, nil
+func (c Coord4D) AsString() string {
+	return fmt.Sprintf("%d/%d/%d/%d", c.X,c.Y,c.Z,c.W)
+}
+
+func makeNeighbours(data []string) map[string]Coord4D {
+
+	ret := make(map[string]Coord4D)
+	z := 0
+	y := 0
+	w := 0
+	for _, line := range data {
+		for x, c := range line {
+			coord := Coord4D{X:x, Y:y, Z:z, W: w}
+			if c == '#' {
+				coord.V = 1
+			}
+			ret[coord.AsString()] = coord
+		}
+		y++
+	}
+
+	for w := -1; w <= 1; w++ {
+		for z := -1; z <= 1; z++ {
+			for y = -1; y <= len(data); y++ {
+				for x := -1; x <= len(data[0]); x++ {
+					coord := Coord4D{X: x, Y: y, Z: z, W: w, V: 0}
+					if _, ok := ret[coord.AsString()]; !ok {
+						ret[coord.AsString()] = coord
+					}
+				}
+			}
+		}
+	}
+	return ret
+}
+
+func partTwo(data []string) (int, error) {
+
+	coords := makeNeighbours(data)
+
+	lastActiveCount := 0
+	for tick := 0; tick < 6; tick++ {
+		activeCount := 0
+		nextCoords := make(map[string]Coord4D)
+		for _, coord := range coords {
+			neighbours := 0
+
+			for w := coord.W-1; w <= coord.W+1; w++ {
+				for z := coord.Z - 1; z <= coord.Z+1; z++ {
+					for y := coord.Y - 1; y <= coord.Y+1; y++ {
+						for x := coord.X - 1; x <= coord.X+1; x++ {
+							ncs := Coord4D{X: x, Y: y, Z: z, W: w}
+							if _, ok := coords[ncs.AsString()]; !ok {
+								coords[ncs.AsString()] = ncs
+							}
+							if coords[ncs.AsString()].V == 1 {
+								neighbours++
+							}
+						}
+					}
+				}
+			}
+			//fmt.Println(neighbours)
+			if coord.V == 1 {
+				if neighbours >= 3 && neighbours <= 4 {
+					newnc := Coord4D{X: coord.X, Y: coord.Y, Z: coord.Z, W: coord.W, V: 1}
+					nextCoords[newnc.AsString()] = newnc
+				} else {
+					newnc := Coord4D{X: coord.X, Y: coord.Y, Z: coord.Z, W: coord.W, V: 0}
+					nextCoords[newnc.AsString()] = newnc
+				}
+			} else {
+				if neighbours == 3 {
+					newnc := Coord4D{X: coord.X, Y: coord.Y, Z: coord.Z, W: coord.W, V: 1}
+					nextCoords[newnc.AsString()] = newnc
+				} else {
+					newnc := Coord4D{X: coord.X, Y: coord.Y, Z: coord.Z, W: coord.W, V: 0}
+					nextCoords[newnc.AsString()] = newnc
+				}
+			}
+
+			if nextCoords[coord.AsString()].V == 1 {
+				activeCount++
+				for w := coord.W-1; w <= coord.W+1; w++ {
+					for z := coord.Z - 1; z <= coord.Z+1; z++ {
+						for y := coord.Y - 1; y <= coord.Y+1; y++ {
+							for x := coord.X - 1; x <= coord.X+1; x++ {
+								nc := Coord4D{X: x, Y: y, Z: z, W: w}
+								if _, ok := nextCoords[nc.AsString()]; !ok {
+									nextCoords[nc.AsString()] = nc
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		coords = nextCoords
+		fmt.Println(activeCount)
+		lastActiveCount = activeCount
+	}
+	return lastActiveCount, nil
 }
 
 func main() {
 	fh, _ := os.Open("input.txt")
 	data, _ := helper.ReadLines(fh, true)
-	ans, err := partOne(data)
+	ans, err := partTwo(data)
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("Part one: %d\n", ans)
-
-	c := helper.Coord3D{1,2,3}
-	d := c.GetNeighbours()
-	for dc, _ := range d {
-		fmt.Println(dc)
-	}
 }
--- helper/coord3d.go
+++ helper/coord3d.go
@@ -6,49 +6,9 @@ type Coord3D struct {
 	X int
 	Y int
 	Z int
+	V int
 }
 
-func (c Coord3D) GetNeighbours() map[Coord3D]bool {
-	nums := make([][]int, 0)
-	for _, n := range []int{c.X,c.Y,c.Z} {
-		nums = append(nums, []int{n-1, n, n+1})
-	}
-	fmt.Println(nums)
-	allm := make(map[Coord3D]bool, 0)
-	for _, p := range nums {
-		np := cartesianProduct(p)
-		for {
-			product := np()
-			if len(product) == 0 {
-				break
-			}
-			cc := Coord3D{product[0], product[1], product[2]}
-			allm[cc] = true
-		}
-	}
-
-	return allm
-}
-
-func cartesianProduct(a []int) func() []int {
-	p := make([]int, len(a))
-	x := make([]int, len(p))
-	return func() []int {
-		p := p[:len(x)]
-		for i, xi := range x {
-			p[i] = a[xi]
-		}
-		for i := len(x) - 1; i >= 0; i-- {
-			x[i]++
-			if x[i] < len(a) {
-				break
-			}
-			x[i] = 0
-			if i <= 0 {
-				x = x[0:0]
-				break
-			}
-		}
-		return p
-	}
+func (c Coord3D) AsString() string {
+	return fmt.Sprintf("%d/%d/%d", c.X,c.Y,c.Z)
 }
